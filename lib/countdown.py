63714fcd6e12afcb9d5b90fbc66a0cbd0e2d6e83
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-09-16 18:32:42 -0400

    passes 2 tests

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import time
+
+def countdown(number):
+    number = number
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -=1
+    else:
+        print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(number):
+    number = number
+    while number > 0:
+        time.sleep(1)
+        print(f'{number} SECOND(S)!')
+        number -=1
+    else:
+        print("HAPPY NEW YEAR!")
